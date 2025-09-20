a24e668491218cc63da920f137144034f112668a
sanwenyukaochi <sanwenyukaochi@outlook.com>
2025-09-20 22:57:52 +0800

    feat: 测试交易

--- src/test/java/com/springframework/jpa/SpringBootJpaSliceTest.java
+++ src/test/java/com/springframework/jpa/SpringBootJpaSliceTest.java
@@ -2,24 +2,39 @@ package com.springframework.jpa;
 
 import com.springframework.jpa.domain.Book;
 import com.springframework.jpa.repositories.BookRepository;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.assertj.core.api.AssertionsForClassTypes;
+import org.springframework.test.annotation.Commit;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @DataJpaTest
 public class SpringBootJpaSliceTest {
     @Autowired
     BookRepository bookRepository;
 
+    @Commit
+    @Order(1)
     @Test
     void testJpaTestSplice() {
         long countBefore = bookRepository.count();
+        AssertionsForClassTypes.assertThat(countBefore).isEqualTo(0);
 
         bookRepository.save(new Book("My Book", "1235555", "Self"));
 
         long countAfter = bookRepository.count();
 
         AssertionsForClassTypes.assertThat(countBefore).isLessThan(countAfter);
+    }
+
+    @Order(2)
+    @Test
+    void testJpaTestSpliceTransaction() {
+        long countBefore = bookRepository.count();
+        AssertionsForClassTypes.assertThat(countBefore).isEqualTo(1);
     }
 }
